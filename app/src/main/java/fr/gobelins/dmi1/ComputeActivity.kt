b42367f30130dba023e2d028ce61462452a5c722
Khadidiatou Dieye <khadijadieye@gmail.com>
2022-10-23 23:41:14 +0200

    last commit

--- app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
@@ -1,15 +1,46 @@
 package fr.gobelins.dmi1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ComputeActivity : AppCompatActivity() {
+    private lateinit var btnCompute: Button
+    private lateinit var firstOperand: EditText
+    private lateinit var secondOperand: EditText
+    private lateinit var operation: EditText
+    private lateinit var result: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.compute_activity)
+        btnCompute = findViewById(R.id.btn_compute)
+        firstOperand = findViewById(R.id.first_operand)
+        secondOperand = findViewById(R.id.second_operand)
+        operation = findViewById(R.id.operation)
+        result=findViewById(R.id.result)
+        //val operation = intent.getStringExtra("operation") ?: "ADD"
 
-        val operation = intent.getStringExtra("operation") ?: "ADD"
-
-
+        //Au click du bouton compute
+        btnCompute.setOnClickListener {
+            var resultat=0
+            var op=true
+            if (operation.getText().toString().lowercase()=="add"){
+                resultat= firstOperand.getText().toString().toInt()+secondOperand.getText().toString().toInt()
+            }else if (operation.getText().toString().lowercase()=="subs"){
+                resultat= firstOperand.getText().toString().toInt()-secondOperand.getText().toString().toInt()
+            }else if (operation.getText().toString().lowercase()=="div"){
+                resultat= firstOperand.getText().toString().toInt()/secondOperand.getText().toString().toInt()
+            }else if (operation.getText().toString().lowercase()=="mult"){
+                resultat= firstOperand.getText().toString().toInt()*secondOperand.getText().toString().toInt()
+            }else{
+                op=false
+                result.text =  "L'opération choisie n'est pas prise en compte."
+            }
+            if (op) {
+                result.text = "Result: $resultat"
+            }
+        }
     }
 }
--- app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
@@ -1,11 +1,66 @@
 package fr.gobelins.dmi1
 
+import android.app.SearchManager
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnHomeCompute: Button
+    private lateinit var btnHomeSOS: Button
+    private lateinit var btnHomeSearch: Button
+    private lateinit var btnHomeShare: Button
+    private lateinit var btnHomeItinerary: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnHomeCompute = findViewById(R.id.btn_home_compute)
+        btnHomeCompute.setOnClickListener {
+            val intent = Intent(this, ComputeActivity::class.java)
+            //intent.extras?.putString("operation", "ADD")
+            startActivity(intent)
+        }
+        // Si l'utilisateur clique sur SOS
+        btnHomeSOS= findViewById(R.id.btn_home_sos)
+        btnHomeSOS.setOnClickListener {
+            val intent= Intent(Intent.ACTION_DIAL)
+            val number="0761728378"
+            intent.data = Uri.parse ("tel:$number")
+            startActivity(intent)
+        }
+
+        // Si l'utilisateur clique sur Search
+        btnHomeSearch= findViewById(R.id.btn_home_search)
+        btnHomeSearch.setOnClickListener {
+            val intent= Intent(Intent.ACTION_WEB_SEARCH)
+            intent.putExtra(SearchManager.QUERY,"https://google.fr")
+            startActivity(intent)
+        }
+
+        // Si l'utilisateur clique sur Share
+        btnHomeShare= findViewById(R.id.btn_home_share)
+        btnHomeShare.setOnClickListener {
+            val intent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Ceci est un test.")
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(intent, null)
+            startActivity(shareIntent)
+        }
+
+
+        // Si l'utilisateur clique sur Itinerary
+        btnHomeItinerary= findViewById(R.id.btn_home_itinerary)
+        btnHomeItinerary.setOnClickListener {
+            val gmmIntentUri =
+                Uri.parse("google.navigation:q=Bureau Vallée Paris V (métro les Gobelins) - papeterie et photocopie, 86 Bd Saint-Marcel, 75005 Paris")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
     }
 }
